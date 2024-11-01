02efc66d5c54c8294b783e76a767ccc220c84ae8
Gourav <gouravgunaga555@gmail.com>
2024-11-01 20:57:17 +0530

    Refactor(PDP):PDP Page Refactored

--- apps/frontend/app/product/[productId]/page.tsx
+++ apps/frontend/app/product/[productId]/page.tsx
@@ -1,173 +1,64 @@
-import {
-  IconBuildingStore,
-  IconChevronDown,
-  IconInfoCircle,
-  IconScale,
-  IconShoppingBag,
-  IconStar,
-  IconStarHalfFilled,
-} from "@tabler/icons-react"
-import Image from "next/image"
+import { IconBuildingStore, IconInfoCircle, IconRuler2, IconShoppingBag } from '@tabler/icons-react'
+import BreadCrumbs from '../../../components/common/breadCrumbs.component'
+import { alternativeImage, breadCrumb, DropDownData, mainImage, skuData, SKUSizeData } from '../../../dummyData'
+import ImageContainer from '../../../components/common/imageContainer.component'
+import Favorite from '../../../components/common/favorite.component'
+import ProductDescrption from '../../../components/common/productDesc.component'
+import SKUSelection from '../../../components/products/skuSelect.component'
+import SelectSize from '../../../components/products/sizeSelection.comonent'
+import TextWithIcon from '../../../components/common/iconedText.component'
+import ReviewStar from '../../../components/common/reviewStar.component'
+import Dropdown from '../../../components/common/dropdown.component'
+import FitGraph from '../../../components/common/fitGraph.component'
+import CustomButton from '../../../components/common/button.component'
 
 const PDP = () => {
-  return (
-    <div className="">
-      <div className="text-[.7rem] text-center p-4">
-        HM.com / Men / Jeans / Baggy&Loose / Loose Jeans
-      </div>
-      <div className="md:flex flex-row-reverse justify-between w-full">
-        <div className="md:hidden flex items-center justify-center">
-          <Image src={"/shirt.jpg"} alt="test" width={420} height={90} />
+    return (
+        <div className="px-4">
+            <BreadCrumbs breadCrumb={breadCrumb} />
+            <div className="md:flex flex-row-reverse w-full ">
+                <div className="md:hidden flex justify-center items-center my-4">
+                    <ImageContainer imgSrc={mainImage.imageSrc} imgAlt={mainImage.imgAlt} size="large">
+                        <Favorite position="top-right" />
+                    </ImageContainer>
+                </div>
+                <div className="pt-6 w-full md:w-1/3 md:px-4">
+                    <ProductDescrption productName="Regular Fit Suite trouser" productPrice="Rs. 2,299.00" />
+                    <SKUSelection SKUData={skuData} />
+                    <SelectSize SKUSizeList={SKUSizeData} />
+                    <TextWithIcon text="Size Guide" className="underline py-4 text-sm">
+                        <IconRuler2 stroke={1} width={20} height={20} />
+                    </TextWithIcon>
+                    <CustomButton buttonText="Add" varient="BlackOne">
+                        <IconShoppingBag stroke={1} />
+                    </CustomButton>
+                    <div className="pt-4">
+                        <TextWithIcon className="text-sm py-1" text="Find in store">
+                            <IconBuildingStore width={20} height={20} stroke={1} />
+                        </TextWithIcon>
+                        <TextWithIcon className="text-sm py-1" text="Delivery Time : 2-7 days">
+                            <IconInfoCircle width={20} height={20} stroke={1} />
+                        </TextWithIcon>
+                    </div>
+                    <div>
+                        <p className="font-extrabold p-1 pb-12">Delivery and Payment</p>
+                    </div>
+                    <ReviewStar /> {/* TODO need to implemet the feature */}
+                    <FitGraph /> {/* TODO need to implemet the feature */}
+                    <div className="flex flex-col w-full pb-2">
+                        {DropDownData.map((i, index) => {
+                            return <Dropdown title={i.title} details={i.Summery} key={index} />
+                        })}
+                    </div>
+                </div>
+                <div className="md:w-2/3 md:grid grid-cols-2 py-4 gap-4 items-center justify-center">
+                    {alternativeImage.map((i, index) => {
+                        return <ImageContainer imgAlt={i.imgAlt} imgSrc={i.imgSrc} key={index} size="large" />
+                    })}
+                </div>
+            </div>
+            {/* TODO: Need to add upsell and cross sell product */}
         </div>
-        <div className="p-2 pt-6  border border-black md:w-1/3">
-          <div className="p-2">
-            <p className="font-bold">Loose Jeans</p>
-            <p className="text-gray-500 text-sm">MRP inclusive of all taxes</p>
-            <p>Rs.2,299.00</p>
-          </div>
-          <div className="flex flex-col gap-4">
-            <p className="text-center text-sm">Light denim blue</p>
-            <div className="flex gap-2 items-center justify-evenly">
-              <div className="border border-black p-1">
-                <Image src={"/shirt.jpg"} alt="test" width={50} height={50} />
-              </div>
-              <div className=" p-1">
-                <Image src={"/shirt.jpg"} alt="test" width={50} height={50} />
-              </div>
-              <div className=" p-1">
-                <Image src={"/shirt.jpg"} alt="test" width={50} height={50} />
-              </div>
-              <div className=" p-1">
-                <Image src={"/shirt.jpg"} alt="test" width={50} height={50} />
-              </div>
-              <div className=" p-1">
-                <Image src={"/shirt.jpg"} alt="test" width={50} height={50} />
-              </div>
-              <div className=" p-1">
-                <Image src={"/shirt.jpg"} alt="test" width={50} height={50} />
-              </div>
-            </div>
-          </div>
-          <div className="md:hidden">
-            <p className="text-sm pl-3">Sizes</p>
-            <div className="flex gap-2 flex-wrap justify-between items-center p-2">
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-              <div className="text-sm py-2 m-1 px-4 border border-gray-400">
-                28/30
-              </div>
-            </div>
-          </div>
-          <div className="text-sm p-4">
-            <p className="flex gap-2 items-center underline p-2">
-              <IconScale scale={2} stroke={1} />
-              Size Guide
-            </p>
-            <div className="border border-black p-1 m-1 mx-4 py-3 text-white bg-black text-center flex items-center justify-center gap-1">
-              <IconShoppingBag scale={2} stroke={1} />
-              Add Button
-            </div>
-          </div>
-          <div className="p-1 flex flex-col gap-1 text-sm">
-            <div className="flex items-center gap-1">
-              <IconBuildingStore scale={2} stroke={1} />
-              <p className="text-sm">Find in store</p>
-            </div>
-            <div className="flex gap-1 items-center">
-              <IconInfoCircle scale={2} stroke={1} />
-              <p className="text-sm">Delivery Time : 2-7 days</p>
-            </div>
-          </div>
-          <div>
-            <p className="font-bold p-1 text-sm pb-12">Delivery and Payment</p>
-            <div className="flex gap-1 py-1">
-              <IconStar scale={1} stroke={1} fill="true" />
-              <IconStar scale={1} stroke={1} fill="true" />
-              <IconStar scale={1} stroke={1} fill="true" />
-              <IconStar scale={1} stroke={1} fill="true" />
-              <IconStarHalfFilled />
-              (2364 Reviews)
-            </div>
-          </div>
-          <div className="border border-black py-8 my-1 mb-12">Size Cart</div>
-          <div className="flex flex-col">
-            <p className="py-2 border-t-2 text-sm border-gray-300">
-              <span className="flex items-center justify-between p-1 font-semibold">
-                DropDown <IconChevronDown />
-              </span>
-            </p>
-            <p className="py-2 border-t-2 text-sm border-gray-300">
-              <span className="flex items-center justify-between p-1 font-semibold">
-                DropDown <IconChevronDown />
-              </span>
-            </p>
-            <p className="py-2 border-t-2 text-sm border-gray-300">
-              <span className="flex items-center justify-between p-1 font-semibold">
-                DropDown <IconChevronDown />
-              </span>
-            </p>
-          </div>
-        </div>
-        <div className="md:flex flex-wrap items-center border border-black md:w-2/3">
-          <div className="flex items-end justify-center">
-            <Image src={"/shirt.jpg"} alt="test" width={400} height={90} />
-          </div>
-          <div className="flex items-end justify-center">
-            <Image src={"/shirt.jpg"} alt="test" width={400} height={90} />
-          </div>
-          <div className="flex items-end justify-center">
-            <Image src={"/shirt.jpg"} alt="test" width={400} height={90} />
-          </div>
-          <div className="flex items-end justify-center">
-            <Image src={"/shirt.jpg"} alt="test" width={400} height={90} />
-          </div>
-        </div>
-      </div>
-      <div>{/* style with and others alos bought part */}</div>
-    </div>
-  )
+    )
 }
 export default PDP
---
+++ apps/frontend/dummyData.ts
@@ -0,0 +1,48 @@
+export const breadCrumb = [
+    { path: 'HM.com', link: '' },
+    { path: 'Shirt', link: '' },
+    { path: 'Men', link: '' },
+]
+
+export const skuData = [
+    { skuName: 'SKU1', imgSrc: '/shirt.jpg', imgAlt: 'Image 1' },
+    { skuName: 'SKU2', imgSrc: '/shirt.jpg', imgAlt: 'Image 2' },
+    { skuName: 'SKU3', imgSrc: '/shirt.jpg', imgAlt: 'Image 3' },
+    { skuName: 'SKU1', imgSrc: '/shirt.jpg', imgAlt: 'Image 1' },
+    { skuName: 'SKU2', imgSrc: '/shirt.jpg', imgAlt: 'Image 2' },
+    { skuName: 'SKU3', imgSrc: '/shirt.jpg', imgAlt: 'Image 3' },
+    { skuName: 'SKU1', imgSrc: '/shirt.jpg', imgAlt: 'Image 1' },
+    { skuName: 'SKU2', imgSrc: '/shirt.jpg', imgAlt: 'Image 2' },
+    { skuName: 'SKU3', imgSrc: '/shirt.jpg', imgAlt: 'Image 3' },
+]
+
+export const alternativeImage = [
+    { imgSrc: '/shirt.jpg', imgAlt: 'Image 1' },
+    { imgSrc: '/shirt.jpg', imgAlt: 'Image 2' },
+    { imgSrc: '/shirt.jpg', imgAlt: 'Image 3' },
+    { imgSrc: '/shirt.jpg', imgAlt: 'Image 1' },
+]
+
+export const mainImage = {
+    imageSrc: '/shirt.jpg',
+    imgAlt: 'Black shirt',
+}
+export const SKUSizeData = ['28/29', '28/29', '28/29', '28/29', '28/29', '28/29', '28/29', '28/29']
+
+export const DropDownData = [
+    {
+        title: 'Description & Fit',
+        Summery:
+            'a quick fox jumps over the lazy dog. a quick fox jumps over the lazy dog.a quick fox jumps over the lazy dog.',
+    },
+    {
+        title: 'Materials',
+        Summery:
+            'a quick fox jumps over the lazy dog. a quick fox jumps over the lazy dog.a quick fox jumps over the lazy dog.',
+    },
+    {
+        title: 'Care guilde',
+        Summery:
+            'a quick fox jumps over the lazy dog. a quick fox jumps over the lazy dog.a quick fox jumps over the lazy dog.',
+    },
+]
