577b407d8f0596394ed93d0df4bdbf7fbe15efcd
Gourav <gouravgunaga555@gmail.com>
2024-11-02 17:19:17 +0530

    feat (button):New button varient added with icon position

--- apps/frontend/components/common/button.component.tsx
+++ apps/frontend/components/common/button.component.tsx
@@ -1,15 +1,24 @@
 interface ButtonTypes extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    varient: 'BlackOne'
+    varient: 'BlackOne' | 'normal'
+    iconPosition?: 'left' | 'right'
     children?: React.ReactNode
     className?: string
     buttonText: string
 }
 
-const CustomButton: React.FC<ButtonTypes> = ({ children, className = '', varient, buttonText }) => {
-    const buttonStyle = varient === 'BlackOne' ? ` w-full py-3 bg-buttonBlack text-white` : ``
+const CustomButton: React.FC<ButtonTypes> = ({
+    children,
+    className = '',
+    varient = 'normal',
+    buttonText,
+    iconPosition = 'left',
+}) => {
+    const buttonStyle =
+        varient === 'BlackOne' ? ` w-full py-3 bg-buttonBlack text-white` : varient === 'normal' ? '' : ''
+    const iconPositionStyle = iconPosition === 'right' ? 'flex-row-reverse' : ''
     return (
         <button className={`${buttonStyle}${className}`}>
-            <span className="flex items-end justify-center gap-2">
+            <span className={`flex items-end justify-center gap-2 ${iconPositionStyle}`}>
                 {children}
                 <p>{buttonText}</p>
             </span>
