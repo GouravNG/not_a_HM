2f5a11f771909f6118e864656b7329a1a240bc8a
Gourav <gouravgunaga555@gmail.com>
2024-11-09 17:49:19 +0530

    Refactor: Header is refactored

--- apps/frontend/components/layout/header.component.tsx
+++ apps/frontend/components/layout/header.component.tsx
@@ -1,28 +1,26 @@
-import ListLinks from "../common/listlink.component"
-import LOGO from "./logo.component"
-import Navigation from "./navigation.component"
-import SearchBar from "../common/search.component"
-import PrimaryFunctions from "./primaryFunction.component"
+import { IconMenu2 } from '@tabler/icons-react'
+import ListLinks from '../common/listlink.component'
+import SearchBar from '../common/search.component'
+import LOGO from './logo.component'
+import PrimaryFunctions from './primaryFunction.component'
 
 const Header = () => {
-  return (
-    <>
-      <header className="flex flex-row-reverse md:flex-col items-center justify-between p-4  md:p-8">
-        <div className="flex justify-end md:justify-between w-full md:pb-6">
-          <div className="hidden md:flex ">
-            <ListLinks />
-          </div>
-          <div className="hidden md:block">
-            <LOGO size={65} />
-          </div>
-          <PrimaryFunctions />
-        </div>
-        <div className="flex w-full items-center justify-between relative">
-          <Navigation />
-          <SearchBar />
-        </div>
-      </header>
-    </>
-  )
+    return (
+        <header className="flex p-3 px-4 md:p-6">
+            <div className="hidden md:block flex-1 pt-4">
+                <ListLinks />
+            </div>
+            <div className="flex-1 flex md:flex-col gap-4 md:justify-center items-center">
+                <IconMenu2 className="md:hidden" stroke={1} />
+                <LOGO size={70} className="hidden md:block" />
+                <LOGO size={43} className="md:hidden" />
+                <nav className="hidden md:block md:mt-4">Navigation</nav>
+            </div>
+            <div className="flex-1 flex flex-col items-end justify-between pt-2">
+                <PrimaryFunctions />
+                <SearchBar />
+            </div>
+        </header>
+    )
 }
 export default Header
--- apps/frontend/components/layout/logo.component.tsx
+++ apps/frontend/components/layout/logo.component.tsx
@@ -1,16 +1,17 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from 'next/image'
+import Link from 'next/link'
 
 type LOGOTypes = {
-  size: number
+    size: number
+    className?: string
 }
 
-const LOGO: React.FC<LOGOTypes> = ({ size }) => {
-  return (
-    <Link href={"/"}>
-      <Image src={"/logo.svg"} alt="logo" width={size} height={size} />
-    </Link>
-  )
+const LOGO: React.FC<LOGOTypes> = ({ size, className = '' }) => {
+    return (
+        <Link href={'/'} className={`${className}`}>
+            <Image src={'/logo.svg'} alt="logo" width={size} height={size} />
+        </Link>
+    )
 }
 
 export default LOGO
